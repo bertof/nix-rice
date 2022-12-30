8a9155a8a4f84867b5c9d8343662679c297ecfe5
Filippo Berto <berto.f@protonmail.com>
2022-12-30 10:40:10 +0100

    Simpler lib callPackage

--- lib.nix
+++ lib.nix
@@ -1,11 +1,12 @@
 { lib, ... }:
 let
+  callPackage = lib.callPackageWith (self // { inherit lib; });
   self = {
-    op = lib.callPackageWith self ./operators.nix { inherit lib; };
-    float = lib.callPackageWith self ./float.nix { inherit lib; };
-    hex = lib.callPackageWith self ./hex.nix { inherit lib; };
-    color = lib.callPackageWith self ./color.nix { inherit lib; };
-    palette = lib.callPackageWith self ./palette.nix { inherit lib; };
+    op = callPackage ./operators.nix { };
+    float = callPackage ./float.nix { };
+    hex = callPackage ./hex.nix { };
+    color = callPackage ./color.nix { };
+    palette = callPackage ./palette.nix { };
   };
 in
 self
